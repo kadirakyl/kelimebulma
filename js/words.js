51adf3dd3ee54c9e900c84ca974561e534f5ad9f
Abdulkadir Akyol <abdulkadirakyolll@gmail.com>
2024-04-30 12:32:34 +0300

    V1

---
+++ js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "sirayet",
+        hint: "Dağılma,bulaşma"
+    },
+    {
+        word: "itidal",
+        hint: "Ölçülülük, soğukkanlılık"
+    },
+    {
+        word: "Vasl",
+        hint: "Kavuşma,birleşme"
+    },
+    {
+        word: "saik",
+        hint: "Sebep"
+    },
+    {
+        word: "yeis",
+        hint: "Üzüntü,umutsuzluk"
+    },
+    {
+        word: "nazenin",
+        hint: "Cilveli, nazlı."
+    },
+    {
+        word: "alenen",
+        hint: "herkesin gözü önünde, açıkça."
+    },
+    {
+        word: "mamafih",
+        hint: "bununla birlikte"
+    },
+    {
+        word: "vahdet",
+        hint: "Birlik, teklik"
+    },
+    {
+        word: "leb",
+        hint: "Dudak"
+    },
+    {
+        word: "berceste",
+        hint: "güzel, latif, seçilmiş, değerli"
+    },
+    {
+        word: "beyhude",
+        hint: "Yararı olmayan, boşuna uğraş"
+    },
+    {
+        word: "girift",
+        hint: "Karmaşık, çapraşık, karışık"
+    },
+    {
+        word: "konçuy",
+        hint: "Soylu kız,prenses"
+    },
+    {
+        word: "vaveyla",
+        hint: "Acının sebep olduğu çığlık"
+    },
+    {
+        word: "mah",
+        hint: "Ay"
+    },
+    {
+        word: "feveran",
+        hint: "Kaynama, coşma, fışkırma"
+    },
+    {
+        word: "velhasıl",
+        hint: "kısaca,özetle"
+    },
+    {
+        word: "zinhar",
+        hint: "Asla"
+    },
+    {
+        word: "amiyane",
+        hint: "Sıradan, bayağı."
+    },
+    {
+        word: "sirayet",
+        hint: "Bulaşma"
+    },
+    {
+        word: "efsunkar",
+        hint: "Büyülü, etkileyici"
+    },
+    {
+        word: "ihtiras",
+        hint: "Aşırı güçlü istek, tutku"
+    },
+]
